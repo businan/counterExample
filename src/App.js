3b534e940652efbaf2e8cd3e4d9dd8dc95d46184
businan <yorulmazburaksinan@gmail.com>
2021-09-11 19:16:14 +0200

    finish lesson with styles

--- src/App.js
+++ src/App.js
@@ -7,12 +7,37 @@ function App() {
     const dispatch = useDispatch();
     return (
         <div className="App">
-            <h2>Counter</h2>
-            <div>
-                <h1>{counter}</h1>
-                <button onClick={() => dispatch(decrease())}>-</button>
-                <button onClick={() => dispatch(reset())}>0</button>
-                <button onClick={() => dispatch(increase())}>+</button>
+            <h1 id="cw">Clarusway</h1>
+            <h2 id="counter">Counter</h2>
+            <div className="main-counter">
+                <h1
+                    className="counter-number"
+                    style={{
+                        color:
+                            counter > 0
+                                ? "green"
+                                : counter === 0
+                                ? "grey"
+                                : "red",
+                    }}
+                >
+                    {counter}
+                </h1>
+                <button
+                    className="decrease"
+                    onClick={() => dispatch(decrease())}
+                >
+                    -
+                </button>
+                <button className="reset" onClick={() => dispatch(reset())}>
+                    0
+                </button>
+                <button
+                    className="increase"
+                    onClick={() => dispatch(increase())}
+                >
+                    +
+                </button>
             </div>
         </div>
     );
